f5c233daca39adcfe57c202fa0c75b29fd102bb5
Ismail Hoxha <ismailhoxha@yahoo.com>
2025-11-07 16:02:19 +0000

    on services dropdown, on hover text caption color should be white

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -247,7 +247,7 @@ const ListItem = React.forwardRef<
         <a
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            "group block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className
           )}
           {...props}
@@ -256,7 +256,7 @@ const ListItem = React.forwardRef<
             {icon}
             <div className="text-sm font-medium leading-none">{title}</div>
           </div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground group-hover:text-white">
             {children}
           </p>
         </a>
