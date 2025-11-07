8d9d440b50a0ffde380cb1f0c7bba4b42474af2e
Blue Square <bluesquareai07@gmail.com>
2025-11-07 18:57:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3380920138-3317b",
+  "appId": "1:745501573999:web:e4d64316587c25ac24de9a",
+  "apiKey": "AIzaSyC0krp9Wvk-D2qO6oQaeXWxzJICXUgLe60",
+  "authDomain": "studio-3380920138-3317b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "745501573999"
+};
